c2e0c111c9bab722ea3ebd6d85c85498341684bc
Hajamahefa3 <hei.hajamahefa11@gmail.com>
2023-12-01 12:41:41 +0300

    change the group Id your project

--- src/main/java/com/app/wellet/Main.java
+++ src/main/java/com/app/wellet/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.app.wellet;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
