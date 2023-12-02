90b61c8b45d748614a1aa16a290794a876af2eb3
Ngitangita <hei.elise.3@gmail.com>
2023-12-02 14:29:51 +0200

    add script

---
+++ src/main/java/com/app/wellet/query/script.sql
@@ -0,0 +1,26 @@
+
+
+CREATE TABLE IF NOT EXISTS "account" (
+     id SERIAL PRIMARY KEY,
+     sold FLOAT,
+     account_type VARCHAR(150),
+    open_date TIMESTAMP,
+    account_number INT
+    );
+
+CREATE TABLE IF NOT EXISTS "devise" (
+    id SERIAL PRIMARY KEY,
+    rate_change FLOAT,
+    devise_symbol VARCHAR(255),
+    account_id INT,
+    FOREIGN KEY (account_id) REFERENCES "account"(id)
+    );
+
+CREATE TABLE IF NOT EXISTS "transaction" (
+     id SERIAL PRIMARY KEY,
+     amount FLOAT,
+     type VARCHAR(255),
+    transaction_date TIMESTAMP,
+    account_id INT,
+    FOREIGN KEY (account_id) REFERENCES "account"(id)
+    );
