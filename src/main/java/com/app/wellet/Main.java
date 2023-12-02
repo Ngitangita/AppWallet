374abafe4836b9200640bc1753c0fd265b9aab6a
Ngitangita <hei.elise.3@gmail.com>
2023-12-02 12:23:19 +0200

    Votre message de validation

--- src/main/java/com/app/wellet/Main.java
+++ src/main/java/com/app/wellet/Main.java
@@ -1,7 +1,10 @@
 package com.app.wellet;
 
 public class Main {
+
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println( System.getenv ("URL") );
+        System.out.println ( System.getenv ("PASSWORD") );
+        System.out.println (System.getenv ("USERNAME") );
     }
 }
