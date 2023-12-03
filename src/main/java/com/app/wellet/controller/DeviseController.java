a1cc6b6b11eab95b3ff478e18e0ed11343fab5b6
Hevitriniavo <hei.tantely@gmail.com>
2023-12-03 17:48:48 +0300

    added the methods crud in deviseController

--- src/main/java/com/app/wellet/controller/DeviseController.java
+++ src/main/java/com/app/wellet/controller/DeviseController.java
@@ -1,6 +1,10 @@
 package com.app.wellet.controller;
 
+import com.app.wellet.DTO.request.DeviseRequestDTO;
+import com.app.wellet.DTO.response.DeviseResponseDTO;
 import com.app.wellet.service.DeviseService;
+
+import java.util.List;
 
 public class DeviseController {
     private final DeviseService deviseService;
@@ -9,5 +13,33 @@ public class DeviseController {
         this.deviseService = deviseService;
     }
 
+
+    public List<DeviseResponseDTO> getDevises() {
+        return this.deviseService.findAll();
+    }
+
+    public List<DeviseResponseDTO> saveDevises(List<DeviseRequestDTO> toSave) {
+        return this.deviseService.saveAll(toSave);
+    }
+
+    public List<DeviseResponseDTO> updateDevise(List<DeviseResponseDTO> toUpdate) {
+        return this.deviseService.updateAll(toUpdate);
+    }
+
+    public DeviseResponseDTO saveDevise(DeviseRequestDTO toSave) {
+        return this.deviseService.saveByEntity(toSave);
+    }
+
+    public DeviseResponseDTO deleteDevise(DeviseResponseDTO toDelete) {
+        return this.deviseService.deleteByEntity ( toDelete );
+    }
+
+    public DeviseResponseDTO getDevise(Long id) {
+        return this.deviseService.findById(id);
+    }
+
+    public DeviseResponseDTO deleteDevise(Long id) {
+        return this.deviseService.delete(id);
+    }
 
 }
